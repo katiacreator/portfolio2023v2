fc898d68edd2b59855fd23d89c217505f2c695ab
katiacreator <kcygibson@gmail.com>
2023-09-09 07:45:06 -0400

    final changes to header styling complete

--- script.js
+++
@@ -1,4 +0,0 @@
-// loops through each letter on hover lighting them up
-let text = document.querySelectorAll('#header-nav a').forEach(text => {
-    text.innerHTML = text.innerText.split('').map((letters,i) => `<span style="transition-delay:${i*100}ms;">${letters}</span>`).join('');
-})
